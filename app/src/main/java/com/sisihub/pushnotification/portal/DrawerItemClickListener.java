0657870058ea4f0d15ee282282e5ab9d2808a580
heronzo <odhiamboherons12@gmail.com>
2017-03-24 17:13:18 +0300

    Portal Updates

---
+++ app/src/main/java/com/sisihub/pushnotification/portal/DrawerItemClickListener.java
@@ -0,0 +1,23 @@
+package com.sisihub.pushnotification.portal;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.support.v7.app.AppCompatActivity;
+
+import com.sisihub.pushnotification.R;
+
+/**
+ * Created by root on 3/24/17.
+ */
+public class DrawerItemClickListener implements ListView.OnItemClickListener {
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+
+}
