3fc65e48830a72efe53e6ac1584cdf7c78b2459c
heronzo <odhiamboherons12@gmail.com>
2017-03-24 12:35:22 +0300

    Portal created

---
+++ app/src/main/java/com/sisihub/pushnotification/portal/Portal.java
@@ -0,0 +1,13 @@
+package com.sisihub.pushnotification.portal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Portal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_portal);
+    }
+}
